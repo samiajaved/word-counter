4717c81f2635a1c3109c603d8ee37b193dca626e
samiajaved <samiaj380@gmail.com>
2024-04-30 03:54:01 -0700

    push my project(word counter)

---
+++ main.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        message: "enter your sentence for counting the word.",
+        type: "input"
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
